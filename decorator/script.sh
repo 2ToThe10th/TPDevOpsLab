88e3a1bbc87aa272e2a23c65238eaf860ce244e0
2ToThe10th <grib.a@phystech.edu>
2019-04-09 16:50:05 +0300

    Decorator part added

---
+++ decorator/script.sh
@@ -0,0 +1,31 @@
+#/bin/bash
+
+sudo apt install make cmake git-all build-essential
+
+git clone https://github.com/akhtyamovpavel/PatternsCollection.git
+
+cd PatternsCollection
+
+echo "[chat]
+token=460383305:AAEH1MB6afXBe2mB09lpfPw0qxXkH9uAvmg
+chat_id=-1001216852554
+[email]
+from=bot.sasha.grib@gmail.com
+to=bot.sasha.grib@gmail.com
+password=qwertyoiuhjk" > ./Decorator/cpp-source/configs/config.ini
+
+sudo apt install libboost-all-dev libgsasl7-dev doxygen gnutls-dev libpthread-stubs0-dev sendmail libssl1.0-dev libcurl4-gnutls-dev
+
+mkdir tmp
+cd tmp
+
+cmake -DON_PI=false -H.. -B.
+make
+
+cd ../bin
+
+echo ""
+echo "------------------"
+echo "start Decorator"
+
+./Decorator
